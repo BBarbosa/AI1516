2b37ac4d8a85e8a5c728ff0f71840a2d77b4c0bd
andre-geraldes <andrejfgeraldes@gmail.com>
2015-11-23 17:00:42 +0000

    New sensor for arduino
    Novo sensor adicionado, é o de temperatura do arduino, não é preciso
    adiciona-lo

---
+++ SmartSensors/src/smartsensors/arduinoTemp.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartsensors;
+
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import java.util.Random;
+
+/**
+ *
+ * @author andregeraldes
+ */
+public class arduinoTemp extends Agent {
+    private boolean sensorState = false;
+    private boolean finished = false;
+    private int last = new Random().nextInt(60);
+    private SerialPortCommunication serial = new SerialPortCommunication();
+
+    @Override
+    protected void takeDown()
+    {
+        super.takeDown();
+
+        try { DFService.deregister(this); }
+        catch (Exception e) {e.printStackTrace();}
+
+        System.out.println("deregistering "+this.getLocalName()+" from service list...");
+    }
+
+    @Override
+    protected void setup()
+    {
+        super.setup();
+
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setName(getLocalName());
+        sd.setType("temp");
+        dfd.addServices(sd);
+
+        try{ DFService.register(this, dfd );}
+        catch (FIPAException fe) { fe.printStackTrace(); }
+
+        System.out.println(this.getLocalName()+" starting!");
+        
+        //Init with arduino
+        serial.initialize();
+
+        this.addBehaviour(new ReceiveBehaviour());
+    }
+
+    public boolean isSensorState() {
+            return sensorState;
+    }
+
+    public void setSensorState(boolean sensorState) {
+            this.sensorState = sensorState;
+    }
+
+    public boolean isFinished() {
+            return finished;
+    }
+
+    public void setFinished(boolean finished) {
+            this.finished = finished;
+    }
+
+    public int getLast() {
+        return last;
+    }
+
+    public void setLast(int last) {
+        this.last = last;
+    }
+
+    public SerialPortCommunication getSerial() {
+        return serial;
+    }
+
+    public void setSerial(SerialPortCommunication serial) {
+        this.serial = serial;
+    }
+    
+    
+
+    private class ReceiveBehaviour extends CyclicBehaviour
+    {
+        @Override
+        public void action() 
+        {
+            ACLMessage msg = receive();
+            if (msg != null) 
+            {            	
+                ACLMessage reply = msg.createReply();
+                reply.setConversationId(msg.getConversationId());
+                
+                if (msg.getPerformative() == ACLMessage.REQUEST)
+                {
+                    if (msg.getContent().equals("shutdown"))
+                    {
+                        System.out.println("sensor "+myAgent.getLocalName()+" exiting...");
+                        setFinished(true);
+                    }
+                   
+                    if (msg.getContent().equals("online"))
+                    {
+                        if (isSensorState())
+                        {
+                            reply.setPerformative(ACLMessage.FAILURE);
+                            myAgent.send(reply);
+                        }
+                        else
+                        {
+                            System.out.println("Sensor "+myAgent.getLocalName()+" is now online!");
+                            reply.setPerformative(ACLMessage.CONFIRM);
+                            myAgent.send(reply);
+                            setSensorState(true);
+                        }
+                    }
+
+                    if (msg.getContent().equals("offline"))
+                    {
+                        if (isSensorState())
+                        {
+                            System.out.println("Sensor "+myAgent.getLocalName()+" is now offline!");
+                            reply.setPerformative(ACLMessage.CONFIRM);
+                            myAgent.send(reply);
+                            setSensorState(false);
+                        }
+                        else
+                        {
+                            reply.setPerformative(ACLMessage.FAILURE);
+                            myAgent.send(reply);
+                        }
+                    }
+
+                    if (msg.getContent().equals("value"))
+                    {
+                        System.out.println(isSensorState());
+                        if (isSensorState())
+                        {
+                            reply.setContent(getSerial().getData() + "");
+                            reply.setPerformative(ACLMessage.INFORM);
+                            myAgent.send(reply);
+                        }
+                        else
+                        {
+                            reply.setPerformative(ACLMessage.FAILURE);
+                            myAgent.send(reply);
+                        }
+                    }
+                }
+                else
+                {
+                    reply.setPerformative(ACLMessage.NOT_UNDERSTOOD);
+                    reply.setContent("Unrecognized request performative. Must be ACLMessage.REQUEST!");
+                    myAgent.send(reply);
+                }
+            }
+
+        if (isFinished())
+            myAgent.doDelete();
+        
+        block();
+        }
+    }
+}
